c4a9ddbf025d9d72c2a690494003d78066a9aad2
weiyang0 <nwy290@gmail.com>
2021-03-07 22:55:43 +0800

    Add lazy load library

--- src/card.js
+++ src/card.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export default function Card(props) {
   const {image} = props
 
   return (
     <div className="card-container">
-      <img src={image.urls.regular} alt={image.alt_description} className="image"/>
+      {/* <img src={image.urls.regular} alt={image.alt_description} className="image"/> */}
+      <LazyLoadImage
+        src={image.urls.regular}
+        alt={image.alt_description}
+        effect="blur"
+      />
       <div className="caption">
         Photo by <a href={`https://unsplash.com/@${image.user.username}?utm_source=your_app_name&utm_medium=referral`} target="_blank" rel="noopener noreferrer">{image.user.name}</a> on <a href="https://unsplash.com/?utm_source=scroll-app&utm_medium=referral" target="_blank" rel="noopener noreferrer">Unsplash</a>
       </div>
